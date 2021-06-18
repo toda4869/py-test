ae49f3b69b0badd87a8b220aad28828dc66a9570
toda4869 <1950454955@qq.com>
2021-06-18 04:03:18 +0000

    add2

--- hello.py
+++ hello.py
@@ -2,6 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
+   
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+@app.logger.debug('A value for debugging')
+@app.logger.warning('A warning occurred (%d apples)', 42)
+@app.logger.error('An error occurred')
